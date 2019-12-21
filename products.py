af81e27f13776c7276cec781518a942a4c5fea1b
Yizhen Zhu <jamilazhu@gmail.com>
2019-12-21 19:26:04 +1100

    add read file

--- products.py
+++ products.py
@@ -1,19 +1,38 @@
 # while loop适用于不知道循环几次
 
-# 2 dimensional 
+# 2 dimensional
+
+# read file
 products = []
+with open('products.csv','r') as f:
+    for line in f:
+        if 'product,price' in line:
+            continue # 跳到下一回的意思，并没有逃出回圈
+        name, price= line.strip().split(',')
+        #把尾巴换行去掉; use , as the split标准，遇到，就切割
+        # split切割完的结果是list
+        products.append([name,price])
+print(products)
+
+# 让使用者输入
 while True:
     name = input('Please enter product name:')
     if name == 'q':
         break
     price = input('Please enter product price:')
-    p = []
-    p.append(name)
-    p.append(price) 
-    products.append(p)
+    products.append([name,price])
 print(products)
 
-# print items in the list
-for product in products:
-    print(product[0], ' price is' , product[1])
+# 印出所有购买记录
+for p in products:
+    print(p[0], ' price is' , p[1])
 
+# write a file --> w
+# read a file --> r
+# save the file as 'f'
+# 写入档案
+with open('products.csv', 'w') as f:
+    f.write('product, price\n')
+    for p in products:
+        f.write(p[0] + ',' + p[1] + '\n')
+# addition of four strings
